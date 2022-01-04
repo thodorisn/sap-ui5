81ad92ac07a60f904eec7ad13fa8edc47da4b456
thodorisn <tnikolakeas@gmail.com>
2022-01-04 21:54:29 +0200

    Parsing text from xml files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# sap-ui5
+# Learning purposes
+
+I'm new to SAP Fiori / ABAP and I'm documenting my journey while I'm reading different kind of sources. The specific repository is based on freeCodeCamp's full course by Brandon.
+My goal is to become an expert through my hard work.
--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-    "sap/m/Text"
+    "sap/ui/core/mvc/XMLView"
 
-], function (Text) {
+], function (XMLView) {
 	"use strict";
 
-    new Text({
-        text: "Hello mates!"
-    }).placeAt("content")
+    XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
 }); 
